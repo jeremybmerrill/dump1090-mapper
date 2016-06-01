5177b4c1118fd927e652933fbc0ae70a2ad7150d
Mike Tigas <mike@tig.as>
2016-06-01 15:43:14 -0400

    add a `MYSQLPORT` option

--- mapify.js
+++ mapify.js
@@ -27,6 +27,7 @@ var linestring_template = {
 
 var connection = mysql.createConnection({
   host     : process.env.MYSQLHOST,
+  port     : process.env.MYSQLPORT,
   user     : process.env.MYSQLUSER,
   password : process.env.MYSQLPASSWORD,
   database : process.env.MYSQLDATABASE || 'dump1090'
